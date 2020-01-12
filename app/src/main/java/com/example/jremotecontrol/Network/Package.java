c38d65359da67229085147db89b64bf3d46ce2ab
eaglx <eaglx.programmer@gmail.com>
2020-01-12 22:50:57 +0100

    update packages body :envelope:

--- app/src/main/java/com/example/jremotecontrol/Network/Package.java
+++ app/src/main/java/com/example/jremotecontrol/Network/Package.java
@@ -1,14 +1,42 @@
 package com.example.jremotecontrol.Network;
 
 public class Package {
+    enum Mod {
+        MOVECURSOR,
+        CLICKMOUSE,
+        INPUTCHAR
+    }
+
+    enum MousBtn {
+        LEFT,
+        RIGHT
+    }
+
+    private Mod m;
     private int mouseXPos;
     private int mouseYPos;
+    private MousBtn mb;
     //private char keyCh;
 
     public Package(int mouseXPos, int mouseYPos){
+        m = Mod.MOVECURSOR;
         this.mouseXPos = mouseXPos;
         this.mouseYPos = mouseYPos;
     }
+
+    public Package(int mb){
+        m = Mod.CLICKMOUSE;
+        if(mb == 1) {
+            this.mb = MousBtn.LEFT;
+        }
+        else if(mb == 2) {
+            this.mb = MousBtn.RIGHT;
+        }
+    }
+
+//    public Package(char c){
+//        m = Mod.INPUTCHAR;
+//    }
 
     public int getMouseYPos() {
         return mouseYPos;
