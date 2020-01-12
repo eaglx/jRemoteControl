fa544dbec34e9a3fcb9d9dbdf8ddbb6973b32adf
eaglx <eaglx.programmer@gmail.com>
2020-01-12 22:40:20 +0100

    update ConnectActivity :iphone:

--- app/src/main/java/com/example/jremotecontrol/ConnectActivity.java
+++ app/src/main/java/com/example/jremotecontrol/ConnectActivity.java
@@ -3,35 +3,77 @@ package com.example.jremotecontrol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.example.jremotecontrol.Abstract.AppConstants;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class ConnectActivity extends AppCompatActivity {
+
+    enum ConnectionStatus {
+        UNCONNECTED,
+        CONNECTING,
+        CONNECTED,
+        FAILED
+    }
+
+    private ConnectionStatus cs;
+    private Button bconn;
+    private ProgressBar progbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
 
-        Button bconn = findViewById(R.id.connbutton);
+        cs = ConnectionStatus.UNCONNECTED;
+
+        progbar = findViewById(R.id.progressBar);
+        progbar.setVisibility(View.GONE);
+
+        bconn = findViewById(R.id.connbutton);
         bconn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AppConstants.getClient();
-                Intent newIntent = new Intent(ConnectActivity.this, ControlActivity.class);
-                startActivity(newIntent);
-
+                if(cs == ConnectionStatus.UNCONNECTED || cs == ConnectionStatus.FAILED) {
+                    progbar.setVisibility(View.VISIBLE);
+                    bconn.setText(R.string.connbuttonInConn);
+                    bconn.setTextColor(Color.parseColor("#f5ba18"));
+                    AppConstants.getClient().Connect();
+                    if(AppConstants.getClient().isConn()) {
+                        bconn.setTextColor(Color.parseColor("#1673B1"));
+                        bconn.setText(R.string.connbuttonDisConn);
+                        cs = ConnectionStatus.CONNECTED;
+                        progbar.setVisibility(View.GONE);
+                        Intent newIntent = new Intent(ConnectActivity.this, ControlActivity.class);
+                        startActivity(newIntent);
+                    }
+                    else{
+                        bconn.setText(R.string.connbuttonErrorConn);
+                        bconn.setTextColor(Color.parseColor("#fc2003"));
+                        cs = ConnectionStatus.FAILED;
+                        progbar.setVisibility(View.GONE);
+                    }
+                }
+                else if(cs == ConnectionStatus.CONNECTED){
+                    AppConstants.getClient().DisConnect();
+                    bconn.setText(R.string.connbuttonConn);
+                }
             }
         });
     }
 
     @Override
     public void onBackPressed() {
-        //client.close();
+        if(AppConstants.getClient().isConn()) {
+            AppConstants.getClient().DisConnect();
+        }
         finishAndRemoveTask();
     }
 }
--- app/src/main/java/com/example/jremotecontrol/Network/Client.java
+++ app/src/main/java/com/example/jremotecontrol/Network/Client.java
@@ -1,20 +1,53 @@
 package com.example.jremotecontrol.Network;
 
-public class Client {
+import android.os.AsyncTask;
+
+public class Client extends AsyncTask<Void, Void, Void> {
 
     private String servIP;
     private String servPort;
+    private boolean isConn;
 
     public Client(){
         servPort = "8090";
+        isConn = false;
     }
 
     public Client(String port){
         servPort = port;
+        isConn = false;
     }
 
-    public boolean Connect(){
+    public void Connect(){
+        isConn = false;
+    }
 
-        return true;
+    public void DisConnect(){
+        isConn = false;
+    }
+
+    public boolean isConn() {
+        return isConn;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        ;
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        ;
     }
 }
